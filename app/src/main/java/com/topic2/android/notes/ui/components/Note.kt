cbcc212a55748e632e5b3303d2fbb3faa57010fb
FantaSalyr <sasha2002salyra@gmail.com>
2023-05-27 21:36:44 +0300

    Completed file Note.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -2,7 +2,12 @@ package com.topic2.android.notes.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,19 +19,28 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable
 fun Note() {
-    Box(
-        modifier = Modifier
-            .size(40.dp)
-            .background(rwGreen)
-    )
-    Text(
-        text = stringResource(id = R.string.header),
-        maxLines = 1
-    )
-    Text(
-        text = stringResource(id = R.string.content),
-        maxLines = 1
-    )
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(rwGreen)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = stringResource(id = R.string.header),
+                maxLines = 1
+            )
+            Text(
+                text = stringResource(id = R.string.content),
+                maxLines = 1
+            )
+        }
+        Checkbox(
+            checked = false,
+            onCheckedChange = {},
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
 
 @Preview
