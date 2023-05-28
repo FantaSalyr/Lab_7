b70562a3074cbc73adf0cba8d1d5e49de75e8782
FantaSalyr <sasha2002salyra@gmail.com>
2023-05-28 03:24:45 +0300

    Changed file SaveNoteScreen.kt

--- app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/SaveNoteScreen.kt
@@ -1,14 +1,17 @@
 package com.topic2.android.notes.ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,13 +30,36 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.res.painterResource
+import com.topic2.android.notes.domain.model.NEW_NOTE_ID
+import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.routing.NotesRouter
+import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.ui.fromHex
 import com.topic2.android.notes.viewmodel.MainViewModel
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
+@ExperimentalMaterialApi
 fun SaveNoteScreen(viewModel: MainViewModel){
+    val noteEntry: NoteModel by viewModel.noteEntry.observeAsState(NoteModel())
 
+    Scaffold(topBar = {
+        val isEditingMode: Boolean = noteEntry.id != NEW_NOTE_ID
+        SaveNoteTopAppBar(
+            isEditingMode = isEditingMode,
+            onBackClick = {
+                NotesRouter.navigateTo(Screen.Notes)
+            },
+            onSaveNoteClick = {},
+            onOpenColorPickerClick = {},
+            onDeleteNoteClick = {}
+        )
+    },
+        content = {}
+    )
 }
 
 @Composable
@@ -90,6 +116,27 @@ private fun SaveNoteTopAppBar(
             }
         }
     )
+}
+
+@Composable
+private fun PickedColor(color: ColorModel){
+    Row (
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.picked_color), modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        NoteColor(
+            color = Color.fromHex(color.hex),
+            size = 40.dp,
+            border = 1.dp,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
 }
 
 
@@ -186,4 +233,10 @@ fun SaveNoteTopAppBarPreview(){
         onOpenColorPickerClick = {  },
         onDeleteNoteClick = {  }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PickedColorPreview(){
+    PickedColor(ColorModel.DEFAULT)
 }
